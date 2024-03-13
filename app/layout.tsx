93810f574cb09addd8f359d98668e81dcac974b0
Atanas Irikev <atanas.irikev@gmail.com>
2024-03-13 15:14:26 +0200

    Changed default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={dmSans.className}>
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
         >
